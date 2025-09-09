b08295f7314bcc8188e71a2a26b0aad31d2553fc
Shubham Meshram <meshram.shubham27@gmail.com>
2025-09-10 01:52:26 +0530

    added new pages

---
+++ src/views/landing-page/LandingPage.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+
+<template>
+  <!-- Landing Page -->
+  <h2>Landing Page</h2>
+</template>
---
+++ src/views/scroller-page/ScrollerPage.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ControlPanel from "../../components/ControlPanel.vue";
+</script>
+
+<template>
+  <ControlPanel />
+</template>
+
+<style scoped></style>
