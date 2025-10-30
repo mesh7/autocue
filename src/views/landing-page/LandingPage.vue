01f0d2c81dfb2116d8dbcf7fcc41a822cad41074
Shubham Meshram <meshram.shubham27@gmail.com>
2025-10-30 23:57:36 +0530

    updated readme.md

--- src/views/landing-page/LandingPage.vue
+++ src/views/landing-page/LandingPage.vue
@@ -1,12 +1,10 @@
 <script setup>
-import Button from "primevue/button";
+import Button from 'primevue/button'
 </script>
 
 <template>
   <section class="min-h-screen flex items-center justify-center px-6">
-    <div
-      class="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 gap-8 items-center"
-    >
+    <div class="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
       <!-- Text Content -->
       <div class="space-y-6">
         <h1 class="text-4xl md:text-5xl font-bold leading-tight">
@@ -17,10 +15,9 @@ import Button from "primevue/button";
           The Timeless Language of Signals
         </h6>
         <p class="mt-6 text-lg text-gray-900 dark:text-white">
-          Whether you're delivering a speech, recording a video, or practicing a
-          presentation, our online teleprompter helps you stay focused and
-          confident. No more fumbling with notes or losing your place—just
-          smooth, readable scrolling text right in your browser.
+          Whether you're delivering a speech, recording a video, or practicing a presentation, our
+          online teleprompter helps you stay focused and confident. No more fumbling with notes or
+          losing your place—just smooth, readable scrolling text right in your browser.
         </p>
         <Button
           label="Get started"
@@ -33,11 +30,7 @@ import Button from "primevue/button";
 
       <!-- Image -->
       <div>
-        <img
-          src="../../assets/dummy.svg"
-          alt="Hero Image"
-          class="w-full rounded-xl shadow-lg"
-        />
+        <img src="../../assets/dummy.svg" alt="Hero Image" class="w-full rounded-xl shadow-lg" />
       </div>
     </div>
   </section>
